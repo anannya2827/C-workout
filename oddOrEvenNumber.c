d52f56f1cb9ec4e498bf1236ef21bf7249a6c911
Anannya Abhi <anannyaabhi2028@cs.sjcetpalai.ac.in>
2025-01-29 07:22:21 +0530

    Create oddOrEvenNumber.c

---
+++ oddOrEvenNumber.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int num1;
+    printf("Enter an integers: ");
+    scanf("%d", &num1);
+    if(num1%2==0){
+        printf("The number is even");   
+    }
+    else{
+        printf("The number is odd");
+    }
+    
+return 0;
+}
